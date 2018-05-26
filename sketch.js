8c149b14e8edb813689079f321381f1cddf7e39f
Pepijnroos <37802996+Pepijnroos@users.noreply.github.com>
2018-05-26 22:58:22 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -82,7 +82,7 @@ function draw(){
   
   fill(255,255,255);
   rect(870,856,406,60);
-  rect(0,750,200,166);
+  rect(0,750,220,166);
   rect(400,0,700,50);
   fill(255,30,255); 
   ellipse(740,591,20,20);  
@@ -96,7 +96,7 @@ function draw(){
   text("Surinam",770,588);
   textSize(20);
   text("Individual slave transports to Berbice, Essequibo and Surinam, 1658-1713",420,35);
-  text("Legend",60,775);
+  text("Legend",70,775);
   image(legendshp, 10, 785); 
   textSize(15); 
   text("Ship",100,815); 
